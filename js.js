436fb93d053cb60db27066dd02b216d1da34affd
Mike Fulcher <mike@drawingablank.me>
2014-11-25 08:56:41 +1300

    Drawing capability
    Includes live path smoothing (a bit choppy though)

--- js.js
+++ js.js
@@ -1,36 +1,54 @@
 (function () {
 
-  var XMLNS = 'http://www.w3.org/2000/svg';
-  var SVG = document.getElementById('svg');
+  var XMLNS, SVG;
+  var _pointer,
+      _currentPath,
+      _mouseDown,
+      _shiftDown,
+      _size = 5,
+      _colour = '#000';
 
-  var pointer = { x: null, y: null };
+  // Constants
+  // ---------
 
-  var currentPath;
+  XMLNS = 'http://www.w3.org/2000/svg';
+  SVG   = document.getElementById('svg');
+  API   = {};
 
-  var _mouseDown = false;
-  var _shiftDown = false;
+  // Globals
+  // -------
+
+  _pointer   = new Point();
+  _mouseDown = false;
+  _shiftDown = false;
+
+  // Events
+  // ------
 
   window.addEventListener('mousedown', function (e) {
     _mouseDown = true;
     _shiftDown = e.shiftKey;
+    _pointer.x = e.pageX;
+    _pointer.y = e.pageY;
   });
 
   [ 'mouseup', 'mouseleave' ].forEach(function (eventType) {
     window.addEventListener(eventType, function (e) {
       _mouseDown = false;
       _shiftDown = e.shiftKey;
+      _currentPath = null;
     });
   });
 
   window.addEventListener('mousemove', function (e) {
-    pointer.x = e.pageX;
-    pointer.y = e.pageY;
+    if (_mouseDown) {
+      _pointer.x = e.pageX;
+      _pointer.y = e.pageY;
+    }
   });
 
-  function mode() {
-    if (_mouseDown && _shiftDown) return 'drag';
-    if (_mouseDown && !_shiftDown) return 'draw';
-  }
+  // Main loop
+  // ---------
 
   requestAnimationFrame(function loop() {
     switch (mode()) {
@@ -47,31 +65,104 @@
     requestAnimationFrame(loop);
   });
 
+  // Functions
+  // ---------
+
+  function mode() {
+    if (_mouseDown && _shiftDown) return 'drag';
+    if (_mouseDown && !_shiftDown) return 'draw';
+  }
+
   function handleDrag() {
-    console.log('panning', pointer);
+    console.log('panning', _pointer);
   }
 
   function handleDraw() {
-    if (!currentPath) newPath();
+    if (!_currentPath) setupPath();
 
-    currentPath.d += ' L' + (pointer.x + ' ' + pointer.y);
-    currentPath.el.setAttribute('d', currentPath.d);
+    // Append current point to the path; but only if
+    // the current pointer is sufficient distance from previous
+    var distance = detectDistance();
+    if (distance > 0) {
+      _currentPath.add(_pointer.clone());
+      _currentPath.raw.push([_pointer.x, _pointer.y]);
+    }
+
+    // Render/re-render the path
+    drawPath();
   }
 
   function handleDefault() {
-    currentPath = null;
+    if (_currentPath) _currentPath = null;
   }
 
-  function newPath() {
-    var element = document.createElementNS(XMLNS, 'path');
+  function setupPath() {
+    _currentPath = new Path();
+    _currentPath.detached = true;
+    _currentPath.add(_pointer.clone());
+    _currentPath.raw = [[ _pointer.x, _pointer.y ]];
+  }
 
-    currentPath = {
-      d: 'M' + (pointer.x + ' ' + pointer.y),
-      el: element
+  function drawPath() {
+    if (_currentPath.detached) attachPath();
+    var simplePath = new Path();
+    simplePath.setSegments(_currentPath.raw);
+    simplePath.simplify(10);
+    var d = compilePathString(simplePath.segments);
+    _currentPath.el.setAttribute('d', d);
+  }
+
+  function compilePathString(segments) {
+    // build a new SVG string for the path
+    var previousSegment = segments[0];
+    var startPoint = segments[0].point;
+    var pathString = 'M' + startPoint.x + ' ' + startPoint.y;
+    segments.slice(1, segments.length).forEach(function(segment) {
+      var x = segment.point.x;
+      var y = segment.point.y;
+      var x1 = previousSegment.point.x + previousSegment.handleOut.x;
+      var y1 = previousSegment.point.y + previousSegment.handleOut.y;
+      var x2 = segment.point.x + segment.handleIn.x;
+      var y2 = segment.point.y + segment.handleIn.y;
+
+      pathString += ('C' + [ x1, y1, x2, y2, x, y ].join(' '));
+      previousSegment = segment;
+    });
+
+    if (segments.length < 2) {
+      pathString += ('L' + startPoint.x + ' ' + startPoint.y);
     }
 
-    element.setAttribute('d', currentPath.d);
-    SVG.insertBefore(element, null);
+    return pathString;
   }
+
+  // TODO: batch the DOM attachment + setAttribute together
+  function attachPath() {
+    _currentPath.detached = false;
+    _currentPath.el = document.createElementNS(XMLNS, 'path');
+    _currentPath.el.setAttribute('stroke', _colour);
+    _currentPath.el.setAttribute('stroke-width', _size);
+    SVG.insertBefore(_currentPath.el, null);
+  }
+
+  function detectDistance() {
+    var point = _currentPath.segments[_currentPath.segments.length-1].point;
+    return point.getDistance(_pointer);
+  }
+
+  // API
+  // ---
+
+  API.colour = function (colour) {
+    if (!colour) return _colour;
+    return _colour = colour;
+  }
+
+  API.size = function (size) {
+    if (!size) return _size;
+    return _size = size;
+  }
+
+  window.qd = API;
 
 })();
