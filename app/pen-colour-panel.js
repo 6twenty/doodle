c757fcc858b1d62ac3612571c5ac96dfe8c5ba9e
Mike Fulcher <michael.fulcher@gmail.com>
2017-04-09 15:20:46 +1200

    Initial colour panel

--- app/pen-colour-panel.js
+++ app/pen-colour-panel.js
@@ -6,7 +6,39 @@ class PenColourPanel extends Panel {
     this.button = button
     this.app = this.button.app
 
+    this.colours = [
+      '#46648e',
+      '#8bbbff',
+      '#89ad48',
+      '#d1d642',
+      '#8c5ba7',
+      '#ca76bf',
+      '#d7503c',
+      '#f49f14',
+      '#fae014',
+      '#000000',
+      '#ffffff'
+    ]
+
     this.render('pen-colour-panel')
+  }
+
+  renderPanel() {
+    this.colours.forEach(hex => {
+      const el = document.createElement('span')
+
+      el.classList.add('pen-colour')
+      el.style.backgroundColor = hex
+      el.onclick = this.penColourClick.bind(this, hex)
+
+      this.el.appendChild(el)
+    })
+  }
+
+  penColourClick(hex) {
+    this.app.pen.colour = hex
+
+    this.close()
   }
 
 }
