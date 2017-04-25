c8067e665a03e410de016e20bc93841c06eed97e
Mike Fulcher <michael.fulcher@gmail.com>
2017-04-25 16:21:48 +1200

    The new canvas button should be always available

--- app/buttons/new-canvas.js
+++ app/buttons/new-canvas.js
@@ -4,8 +4,6 @@ class NewCanvas extends Button {
     super(app)
 
     this.el.onclick = this.click.bind(this)
-
-    this.on('canvas:save', this.update)
   }
 
   render() {
@@ -14,23 +12,11 @@ class NewCanvas extends Button {
     this.el.id = 'new-canvas'
     this.el.classList.add('button')
 
-    if (this.app.canvas.paths.length === 0) {
-      this.el.classList.add('hidden')
-    }
-
     this.app.el.appendChild(this.el)
   }
 
   click() {
     this.app.reset()
-  }
-
-  update(data) {
-    if (data.paths.length > 0) {
-      this.el.classList.remove('hidden')
-    } else {
-      this.el.classList.add('hidden')
-    }
   }
 
 }
