3a382c33c5e4bab073834a09c66101b45089bbc3
Mike Fulcher <michael.fulcher@gmail.com>
2017-04-29 22:43:58 +1200

    Simplify the compiler to just use the file's modified time, like Rails

--- compile.rb
+++ compile.rb
@@ -6,7 +6,7 @@ class Compiler
     @index_html.each_line do |line|
       file = line[/<script src="(.*?)"/, 1]
       file ||= line[/<link rel="stylesheet" href="(.*?)"/, 1]
-      hashify(line, file) if file
+      cachebust(line, file) if file
     end
 
     File.write('index.html', @index_html)
@@ -14,10 +14,10 @@ class Compiler
 
   private
 
-  def hashify(line, file)
+  def cachebust(line, file)
     path = file.sub(/\?.*$/, '')
-    hash = `md5 -q #{path}`.chomp
-    new_line = line.sub(file, "#{path}?#{hash}")
+    timestamp = File.mtime(path).to_i
+    new_line = line.sub(file, "#{path}?#{timestamp}")
 
     @index_html.sub!(line, new_line)
   end
