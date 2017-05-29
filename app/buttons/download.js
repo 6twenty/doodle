1f19d1bdd7a3b0d1c57a0a85956ed2ac25206446
Mike Fulcher <michael.fulcher@gmail.com>
2017-05-29 22:18:46 +1200

    Update the download handler
    - Ensure eraser paths are rendered correctly
    - Use object URLs
    - Ensure rendered image is actual size

--- app/buttons/download.js
+++ app/buttons/download.js
@@ -23,29 +23,72 @@ class Download extends Button {
     this.app.el.appendChild(this.el)
   }
 
-  click() {
+  click(e) {
+    if (this.el.href) {
+      return
+    }
+
+    e.preventDefault()
+
+    const canvas = this.buildCanvas()
+
+    canvas.toBlob(blob => {
+      this.el.href = URL.createObjectURL(blob)
+      this.el.click()
+      URL.revokeObjectURL(this.el.href)
+      this.el.removeAttribute('href')
+    })
+  }
+
+  buildCanvas() {
     const canvas = document.createElement('canvas')
     const ctx = canvas.getContext('2d')
     const box = this.app.canvas.boundingBox(this.app.canvas.paths)
-    const margin = Math.max(box.width * 0.1, box.height * 0.1)
+    const svg = document.querySelector('svg')
 
-    canvas.width = (box.width + margin) * window.devicePixelRatio
-    canvas.height = (box.height + margin) * window.devicePixelRatio
+    let matrix = svg.createSVGMatrix()
+
+    matrix = matrix.scale(window.devicePixelRatio)
+    matrix = matrix.translate(box.width / 2, box.height / 2)
+    matrix = matrix.translate(-(box.left + (box.width / 2)), -(box.top + (box.height / 2)))
+
+    canvas.style.height = `${box.height}px`
+    canvas.style.width = `${box.width}px`
+
+    canvas.height = box.height * window.devicePixelRatio
+    canvas.width = box.width * window.devicePixelRatio
+
+    ctx.setTransform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f)
+
+    // White background
     ctx.fillStyle = 'white'
-
-    ctx.scale(window.devicePixelRatio, window.devicePixelRatio)
-    ctx.translate(-(box.left - (margin / 2)), -(box.top - (margin / 2)))
-    ctx.fillRect(box.left - margin, box.top - margin, canvas.width + margin, canvas.height + margin)
+    ctx.fillRect(box.left, box.top, canvas.width, canvas.height)
 
     this.app.canvas.layers.forEach(layer => {
-      const layerCtx = layer.ctx
+      if (layer.paths.length === 0) {
+        return
+      }
 
-      layer.ctx = ctx
-      layer.redraw()
+      const layerCanvas = document.createElement('canvas')
+      const layerCtx = layerCanvas.getContext('2d')
+      const layerCtxOriginal = layer.ctx
+
+      layerCanvas.width = canvas.width
+      layerCanvas.height = canvas.height
+
+      // Same transformation as the main canvas
+      layerCtx.setTransform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f)
+
+      // Draw paths to the new context
       layer.ctx = layerCtx
+      layer.redraw()
+      layer.ctx = layerCtxOriginal
+
+      // Render onto the main canvas
+      ctx.drawImage(layerCanvas, box.left, box.top, box.width, box.height)
     })
 
-    this.el.href = canvas.toDataURL('image/png')
+    return canvas
   }
 
   update(data) {
