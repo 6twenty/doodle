e0e43fde45ac650b9fe79d8e023fdef0c114b192
Mike Fulcher <michael.fulcher@gmail.com>
2017-06-09 21:52:31 +1200

    Move the png snapshot code into Canvas

--- web/app/buttons/download.js
+++ web/app/buttons/download.js
@@ -27,9 +27,13 @@ class Download extends Button {
       return
     }
 
+    if (this.app.canvas.paths.length === 0) {
+      return
+    }
+
     e.preventDefault()
 
-    const canvas = this.buildCanvas()
+    const canvas = this.app.canvas.snapshot()
 
     canvas.toBlob(blob => {
       this.el.href = URL.createObjectURL(blob)
@@ -37,57 +41,6 @@ class Download extends Button {
       URL.revokeObjectURL(this.el.href)
       this.el.removeAttribute('href')
     })
-  }
-
-  buildCanvas() {
-    const canvas = document.createElement('canvas')
-    const ctx = canvas.getContext('2d')
-    const box = this.app.canvas.boundingBox(this.app.canvas.paths)
-    const svg = document.querySelector('svg')
-
-    let matrix = svg.createSVGMatrix()
-
-    matrix = matrix.scale(window.devicePixelRatio)
-    matrix = matrix.translate(box.width / 2, box.height / 2)
-    matrix = matrix.translate(-(box.left + (box.width / 2)), -(box.top + (box.height / 2)))
-
-    canvas.style.height = `${box.height}px`
-    canvas.style.width = `${box.width}px`
-
-    canvas.height = box.height * window.devicePixelRatio
-    canvas.width = box.width * window.devicePixelRatio
-
-    ctx.setTransform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f)
-
-    // White background
-    ctx.fillStyle = 'white'
-    ctx.fillRect(box.left, box.top, canvas.width, canvas.height)
-
-    this.app.canvas.layers.forEach(layer => {
-      if (layer.paths.length === 0) {
-        return
-      }
-
-      const layerCanvas = document.createElement('canvas')
-      const layerCtx = layerCanvas.getContext('2d')
-      const layerCtxOriginal = layer.ctx
-
-      layerCanvas.width = canvas.width
-      layerCanvas.height = canvas.height
-
-      // Same transformation as the main canvas
-      layerCtx.setTransform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f)
-
-      // Draw paths to the new context
-      layer.ctx = layerCtx
-      layer.redraw()
-      layer.ctx = layerCtxOriginal
-
-      // Render onto the main canvas
-      ctx.drawImage(layerCanvas, box.left, box.top, box.width, box.height)
-    })
-
-    return canvas
   }
 
   update(data) {
--- web/app/canvas/canvas.js
+++ web/app/canvas/canvas.js
@@ -339,4 +339,55 @@ class Canvas extends Eventable {
     }
   }
 
+  snapshot() {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const box = this.boundingBox(this.paths)
+    const svg = document.querySelector('svg')
+
+    let matrix = svg.createSVGMatrix()
+
+    matrix = matrix.scale(window.devicePixelRatio)
+    matrix = matrix.translate(box.width / 2, box.height / 2)
+    matrix = matrix.translate(-(box.left + (box.width / 2)), -(box.top + (box.height / 2)))
+
+    canvas.style.height = `${box.height}px`
+    canvas.style.width = `${box.width}px`
+
+    canvas.height = box.height * window.devicePixelRatio
+    canvas.width = box.width * window.devicePixelRatio
+
+    ctx.setTransform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f)
+
+    // White background
+    ctx.fillStyle = 'white'
+    ctx.fillRect(box.left, box.top, canvas.width, canvas.height)
+
+    this.layers.forEach(layer => {
+      if (layer.paths.length === 0) {
+        return
+      }
+
+      const layerCanvas = document.createElement('canvas')
+      const layerCtx = layerCanvas.getContext('2d')
+      const layerCtxOriginal = layer.ctx
+
+      layerCanvas.width = canvas.width
+      layerCanvas.height = canvas.height
+
+      // Same transformation as the main canvas
+      layerCtx.setTransform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f)
+
+      // Draw paths to the new context
+      layer.ctx = layerCtx
+      layer.redraw()
+      layer.ctx = layerCtxOriginal
+
+      // Render onto the main canvas
+      ctx.drawImage(layerCanvas, box.left, box.top, box.width, box.height)
+    })
+
+    return canvas
+  }
+
 }
